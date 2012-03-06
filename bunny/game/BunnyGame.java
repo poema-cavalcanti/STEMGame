eb1f72ce19965b2d38c486c5611d250b2eb03e7c
Poema Cavalcanti <poema.campos@gmail.com>
2012-03-05 21:06:42 -0800

    update

--- bunny/game/BunnyGame.java
+++ bunny/game/BunnyGame.java
@@ -7,6 +7,7 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
  
@@ -14,6 +15,7 @@ public class BunnyGame extends BasicGame
 {
 	private TiledMap grassMap;
 	private Animation sprite, up, down, left, right;
+	private float x = 34f, y = 34f;
 	
     public BunnyGame()
     {
@@ -32,16 +34,16 @@ public class BunnyGame extends BasicGame
     	* By setting it to false animation will update only when
     	* the user presses a key.
     	*/
-    	Image [] movementUp = {new Image("data/bunny_back.gif"), new Image("data/bunny_back.gif")};
+    	Image [] movementUp = {new Image("bunny/data/bunny_back.gif"), new Image("bunny/data/bunny_back.gif")};
     	up = new Animation(movementUp, duration, false);
     	
-    	Image [] movementDown = {new Image("data/bunny_front.gif"), new Image("data/bunny_front.gif")};
+    	Image [] movementDown = {new Image("bunny/data/bunny_front.gif"), new Image("bunny/data/bunny_front.gif")};
     	down = new Animation(movementDown, duration, false);
     	
-    	Image [] movementLeft = {new Image("data/bunny_side.gif"), new Image("data/bunny_side.gif")};
+    	Image [] movementLeft = {new Image("bunny/data/bunny_side.gif"), new Image("bunny/data/bunny_side.gif")};
     	left = new Animation(movementLeft, duration, false);
     	
-    	Image [] movementRight = {new Image("data/bunny_side.gif"), new Image("data/bunny_side.gif")};
+    	Image [] movementRight = {new Image("bunny/data/bunny_side.gif"), new Image("bunny/data/bunny_side.gif")};
     	right = new Animation(movementRight, duration, false); 
 
     	// Original orientation of the sprite. It will look right.
@@ -54,7 +56,33 @@ public class BunnyGame extends BasicGame
     public void update(GameContainer gc, int delta) 
 			throws SlickException     
     {
- 
+    	Input input = gc.getInput();
+    	if (input.isKeyDown(Input.KEY_UP))
+    	{
+    	    sprite = up;
+    	    sprite.update(delta);
+    	    // The lower the delta the slowest the sprite will animate.
+    	    y -= delta * 0.1f;
+    	}
+    	else if (input.isKeyDown(Input.KEY_DOWN))
+    	{
+    	    sprite = down;
+    	    sprite.update(delta);
+    	    y += delta * 0.1f;
+    	}
+    	else if (input.isKeyDown(Input.KEY_LEFT))
+    	{
+    	    sprite = left;
+    	    sprite.update(delta);
+    	    x -= delta * 0.1f;
+    	}
+    	else if (input.isKeyDown(Input.KEY_RIGHT))
+    	{
+    	    sprite = right;
+    	    sprite.update(delta);
+    	    x += delta * 0.1f;
+    	}
+
     }
     
     // renders what is on screen
@@ -62,6 +90,7 @@ public class BunnyGame extends BasicGame
 			throws SlickException 
     {
     	grassMap.render(0,0);
+    	sprite.draw((int)x, (int)y);
     }
  
     public static void main(String[] args) 
